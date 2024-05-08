1bffbc413dd6694b1a23037ef61d6fe7b57f97df
Samuel Rojas <150297530+SamuelRojas02@users.noreply.github.com>
2024-05-08 14:00:57 -0500

    Create Graficos.R

---
+++ Graficos.R
@@ -0,0 +1,72 @@
+library(shiny)
+library(shinydashboard)
+
+# Cargar los datos de la base de datos
+
+# Definir la interfaz de usuario (UI)
+ui <- dashboardPage(
+  dashboardHeader(title = "Gemelos"),
+  dashboardSidebar(
+    sidebarMenu(
+    #diseño del icono de graficos
+      menuItem("Gráficos", tabName = "graficos", icon = icon("chart-line"))
+    )
+  ),
+  dashboardBody(
+    #botones de el dashboard 
+    tabItems(
+      tabItem(
+        tabName = "graficos",
+        sidebarPanel(
+          selectInput("var_x_1", "Seleccionar variable para el eje Y, para el gemelo1:",
+                      choices = c("DEDUC1", "AGE", "HRWAGEH", "WHITEH", "MALEH", "EDUCH"," DMARRIED", "DUNCOV")),
+          actionButton("plotBtn_1", "Generar Gráfico gemelo 1"),
+          
+          selectInput("var_x_2", "Seleccionar variable para el eje Y, para el gemelo2:",
+                      choices = c("WHITEL","AGESQ",  "MALEL", "EDUCL", "DEDUC2", "DTEN", "DMARRIED", "DUNCOV")),
+          actionButton("plotBtn_2", "Generar Gráfico gemelo 2")
+        ),
+        mainPanel(
+          plotOutput("dotchart_1"),
+          plotOutput("dotchart_2")
+        )
+      )
+    )
+  )
+)
+
+# Definir la lógica del servidor
+server <- function(input, output) {
+  # Gráfico 1
+  observeEvent(input$plotBtn_1, {
+    req(input$var_x_1)
+    selected_variable_x_1 <- input$var_x_1
+    
+    output$dotchart_1 <- renderPlot({
+      dotchart(
+        as.numeric(twins_2_[[selected_variable_x_1]]),
+        main = paste("Dotchart de", selected_variable_x_1),
+        xlab = selected_variable_x_1,
+        ylab = "DLHRWAGE"
+      )
+    })
+  })
+  
+  # Gráfico 2
+  observeEvent(input$plotBtn_2, {
+    req(input$var_x_2)
+    selected_variable_x_2 <- input$var_x_2
+    
+    output$dotchart_2 <- renderPlot({
+      dotchart(
+        as.numeric(twins_2_[[selected_variable_x_2]]),
+        main = paste("Dotchart de", selected_variable_x_2),
+        xlab = selected_variable_x_2,
+        ylab = "HRWAGEL"
+      )
+    })
+  })
+}
+
+# Ejecutar la aplicación Shiny
+shinyApp(ui = ui, server = server)
