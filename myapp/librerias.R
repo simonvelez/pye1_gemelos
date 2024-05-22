b9092b0c605baa9a248311e6bc334d538bc9335e
Isabellagutierrezme <isabellagutierrez9461@gmail.com>
2024-05-22 11:39:25 -0500

    Update librerias.R

--- myapp/librerias.R
+++ myapp/librerias.R
@@ -1,10 +1,11 @@
 # Librerías necesarias para ejecutar el aplicativo.
 
 install.packages("shiny")
-install.packages("shinydashboard")
+
 install.packages("ggplot2")
 install.packages("httpuv")
 install.packages("shinylive")
 install.packages("DT")
 install.packages("shinyjs")
 install.packages("ggplot2")
+install.packages("shinydashboard")
