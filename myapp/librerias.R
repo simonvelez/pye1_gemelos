4b0f14c7a0b8ebe6d3ffb2daa94b0de75a3e14b1
Isabellagutierrezme <167822760+Isabellagutierrezme@users.noreply.github.com>
2024-05-24 11:52:16 -0500

    Update librerias.R

--- myapp/librerias.R
+++ myapp/librerias.R
@@ -3,5 +3,7 @@ library(shinydashboard) # Estructura de la página
 library(shinyjs)  # Para usar funciones de JavaScript (botones)
 library(ggplot2) # Crear gráficos y ponerlos bonitos
 library(dplyr) #calcular media, moda, mediana y demas
+library(shinylive)
+library(httpuv)
 
 ## PENDIENTE
