0b9b13c80e8b512badb4e5395447a5cb51daaac8
Isabellagutierrezme <167822760+Isabellagutierrezme@users.noreply.github.com>
2024-05-24 11:51:57 -0500

    Update librerias.R

--- myapp/librerias.R
+++ myapp/librerias.R
@@ -1,3 +1,7 @@
 # Librerías necesarias para ejecutar el aplicativo.
+library(shinydashboard) # Estructura de la página
+library(shinyjs)  # Para usar funciones de JavaScript (botones)
+library(ggplot2) # Crear gráficos y ponerlos bonitos
+library(dplyr) #calcular media, moda, mediana y demas
 
 ## PENDIENTE
